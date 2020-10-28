1697179ecbf0d5bc09248f0595f631bee70151bc
Natalia Martian <natalia.martian@gmail.com>
2020-10-28 04:00:45 +0000

    Working code for extraction function

--- without.js
+++ without.js
@@ -24,22 +24,26 @@ const without = function(source, itemsToRemove) {
   let extractedArray = [];
   //if arrays are equal, return empty array
   if (eqArrays(source, itemsToRemove)) {
-    console.log(`${extractedArray} is empty`);
     return extractedArray;
   } else {
   //loop through arrays
     for (let i = 0; i < source.length; i++) {
+      //suppose the elements of the two arrays are not equal
+      let elementsAreSame = false;
       for (let j = 0; j < itemsToRemove.length; j++) {
-        if ((source[i] !== itemsToRemove[j])) {
-          extractedArray.push(source[i]);
+        if ((source[i] === itemsToRemove[j])) {
+          elementsAreSame = true;
+          break;
         }
+      }
+      if (elementsAreSame === false) {
+        extractedArray.push(source[i]);
       }
     }
     return extractedArray;
   }
 }
 
-//console.log(without([1, 2, 3], [1, 2, 3]));
-//console.log(without([1, 2, 3], [1]));
+console.log(without([1, 2, 3], [1, 2, 3]));
+console.log(without([1, 2, 3], [1]));
 console.log(without(['1', '2', '3'], [1, 2, '3']));
-//console.log(eqArrays(['1', '2', '3'], [1, 2, '3']));
