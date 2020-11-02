da9bb8dfc8bb2b9ba4ad950105caba8df945fd4c
Natalia Martian <natalia.martian@gmail.com>
2020-11-02 23:55:45 +0000

    Refactored head, middle and tail test files to incorporate Mocha & Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,29 @@
 const middle = require('../middle');
-const assertEqualArrays = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-//make sure the middle function is returning the good values
-console.log(assertEqualArrays(middle([1]), []));
-console.log(assertEqualArrays(middle([1, 2]), []));
-console.log(assertEqualArrays(middle([1, 2, 3, 4, 5]), [3]));
-console.log(assertEqualArrays(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]));
-console.log(assertEqualArrays(middle([1, 2, '3', 4, 5]), ['3']));
-console.log(assertEqualArrays(middle([1, 2, 3, 'four', '5', 6, 7, 8]), ['four', '5']));
+describe ('#middle', function() {
+
+  it('returns empty array when given a single element array', function() {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it('returns empty array when passed an array with two elements', function() {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it('returns an array with one element for arrays with odd number of elements', function() {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it('returns an array with two elements for arrays with even number of elements', function() {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
+
+  it('returns array with same type of value and one element(odd length)', function() {
+    assert.deepEqual(middle([1, 2, '3', 4, 5]), ['3']);
+  });
+
+  it('returns array with same type of value and two elements(even length)', function() {
+    assert.deepEqual(middle([1, 2, 3, 'four', '5', 6, 7, 8]), ['four', '5']);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,20 +1,49 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-const assertEqualArrays = require('../assertArraysEqual');
+// const assertEqual = require('../assertEqual');
+// const assertEqualArrays = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 const wordsArray = ["Hello", "Lighthouse", "Labs"];
 const numbersArray = [1, 2, 3, 4, 5, 6];
 const oneElementArray = ['🥦'];
 const emptyArray = [];
 
-//make sure the original array was not modified
-assertEqual(wordsArray.length, 3);
-assertEqual(numbersArray.length, 6);
-assertEqual(oneElementArray.length, 1);
-assertEqual(emptyArray.length, 0);
+describe ('#tail', function() {
+  context ('does not modify the original array\n' , function() {
 
-//make sure the tail function is doing its job
-assertEqualArrays(tail(wordsArray), ["Lighthouse", "Labs"]);
-assertEqualArrays(tail(numbersArray), [2, 3, 4, 5, 6]);
-assertEqualArrays(tail(oneElementArray), []);
-assertEqualArrays(tail(emptyArray), []);
+    it ('returns 3 when wordsArray is input', function() {
+      assert.strictEqual(wordsArray.length, 3);
+    });
+
+    it ('returns 6 when numbersArray is input', function() {
+      assert.strictEqual(numbersArray.length, 6);
+    });
+
+    it ('returns 1 when oneElementArray is input', function() {
+      assert.strictEqual(oneElementArray.length, 1);
+    });
+
+    it ('returns 0 when emptyArray is input\n', function() {
+      assert.strictEqual(emptyArray.length, 0);
+    });
+  });
+
+  context('function works as expected (returns what it should)\n', function() {
+
+    it('returns ["Lighthouse", "Labs"] when wordsArray is input', function() {
+      assert.deepEqual(tail(wordsArray), ["Lighthouse", "Labs"]);
+    });
+
+    it('returns [2, 3, 4, 5, 6] when numbersArray is input', function() {
+      assert.deepEqual(tail(numbersArray), [2, 3, 4, 5, 6]);
+    });
+  
+    it('returns empty array when oneElementArray is input', function() {
+      assert.deepEqual(tail(oneElementArray), []);
+    });
+  
+    it('returns empty array when emptyArray is input', function() {
+      assert.deepEqual(tail(emptyArray), []);
+    });
+  })
+});
